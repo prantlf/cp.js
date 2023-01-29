711689de3e457407bab378c5e7621d43c8352216
Ferdinand Prantl <prantlf@gmail.com>
2023-01-29 01:11:40 +0100

    fix: Fix -H argument, add other missing arguments
    * Argument `-H` follows only symlinks supplied on the command-line.
    * Add arguments `-a`, `-d`, `-L`, `-P`.
    * If arguments end with a slash (/), the children of such arguments (directories) will be compied instead of the argument (directory) itself.
    
    BREAKING CHANGE: The argument `-H` dereferences only symlinks supplied on the command-line. Not symlinks found in the subdirectories any more. If you want to dereference all symlinks, use the argument `L`. Also, if an argument ends with a slash (/), its children will be copied instead. If you want to copy the whole directory, do not end its name on the command line by a slash (/).

--- README.md
+++ README.md
@@ -3,16 +3,35 @@
 [![Latest version](https://img.shields.io/npm/v/@unixcompat/cp.js)
  ![Dependency status](https://img.shields.io/librariesio/release/npm/@unixcompat/cp.js)
 ](https://www.npmjs.com/package/@unixcompat/cp.js)
+[![Coverage](https://codecov.io/gh/prantlf/cp.js/branch/master/graph/badge.svg)](https://codecov.io/gh/prantlf/cp.js)
 
 Copies files or directories like the `cp` command.
 
 There are multi-platform file-system commands compatible with `cp` from UN*X implemented for Node.js in JavaScript, like [ncp], [cpy-cli], [cpx] or [copyfiles], but they have different interface and different behaviour than the `cp` command. Instead of reusing the knowledge of the `cp` command, you would have to learn their new interface. This project aims to provide the well-known interface of the `cp` command.
 
-See also other commands compatible with their counterparts from UN*X - [cat.js], [mkdir.js] and [rm.js].
+This package offers only command-line interface, because programmatic interface is provided by [`cp`] and [`copyFile`] from [node:fs]. See also other commands compatible with their counterparts from UN*X - [cat.js], [mkdir.js] and [rm.js].
+
+## Synopsis
+
+The following scripts from `package.json` won't work on Windows:
+
+    rm -rf dist
+    mkdir -p dist
+    cat src/umd-prolog.txt src/code.js src/umd-epilog.txt > dist/index.umd.js
+    cp src/index.d.ts dist
+
+Replace them with the following ones, which run on any operating system which is supported by Node.js:
+
+    rm.js -rf dist
+    mkdir.js -p dist
+    cat.js src/umd-prolog.txt src/code.js src/umd-epilog.txt > dist/index.umd.js
+    cp.js src/index.d.ts dist
+
+Notice that the only difference is the suffix `.js` behind the command names.
 
 ## Installation
 
-This module can be installed in your project using [NPM], [PNPM] or [Yarn]. Make sure, that you use [Node.js] version 14.8 or newer.
+This module can be installed in your project using [NPM], [PNPM] or [Yarn]. Make sure, that you use [Node.js] version 16.15 or newer.
 
 ```sh
 $ npm i -D @unixcompat/cp.js
@@ -22,24 +41,91 @@ $ yarn add -D @unixcompat/cp.js
 
 ## Command-line Interface
 
-    Usage: cp.js [-DfHnpRrv] [--] src... dest
+See also `man cp` for the original [POSIX documentation] or for the extended [Linux implementation].
+
+    Usage: cp.js [-DfHLnPpRrv] [--] src... dest
 
     Options:
-      -c|--cwd <dir>      directory to start looking for the source files in
-      -D|--dry-run        only print path of each source file or directory
-      -f|--force          removes the destination if not writable
-      -H|--dereference    follows symbolic links, so that the files are copied
-      -n|--no-clobbering  prevents accidentally overwriting any files
-      -p|--preserve       preserves timestamps of the source files
-      -R|--recursive      copy directories recursively (also -r)
-      -v|--verbose        print path of each copied file or directory
-      -V|--version        print version number
-      -h|--help           print usage instructions
+      -c|--cwd <dir>              directory to start looking for the source files
+      -D|--dry-run                only print paths of source files or directories
+      -a|--archive                same as -dpR
+      -d                          the same as -P
+      -f|--force                  removes the destination if not writable
+      -H|--dereference-args       follow command-line symbolic links in src
+      -L|--dereference            always follow symbolic links in src
+      -n|--no-clobbering          prevents accidentally overwriting any files
+      -P|--no-dereference         never follow symbolic links in src
+      -p|--preserve[=timestamps]  preserve timestamps of the source files
+      -r                          the same as -R
+      -R|--recursive              copy directories recursively
+      -v|--verbose                print path of each copied file or directory
+      -V|--version                print version number
+      -h|--help                   print usage instructions
 
     Examples:
       $ cp.js prog.js prog.bak
       $ cp.js jones smith /home/nick/clients
-      $ cp.js -R /home/nick/clients /home/nick/customers
+      $ cp.js -R /home/nick/clients/ /home/nick/customers
+
+## Differences
+
+The following options are specific to this command:
+
+    -D|--dry-run    only print path of each file or directory
+    -c|--cwd <dir>  directory to start looking for the source files
+
+Also, the arguments may be [BASH patterns]. The pattern matching will ignore symbolic links. The argument `-c|--cwd` will be used only as a base directory to expand the BASH patterns in.
+
+The following options from the POSIX version are not supported:
+
+    -i    write a prompt to standard error before copying to any existing
+          non-directory destination file. If the response from the standard
+          input is affirmative, the copy shall be attempted; otherwise,
+          it shall not.
+    -p    duplicate the time of last data modification and time of last
+          access, the user ID and group ID, the file permission bits
+          and the S_ISUID and S_ISGID bits of each source file
+          in the corresponding destination file
+
+The option `-p` is supported, but will preserve only timestamps, no other attributes of the source files in the destination ones.
+
+The following options from the Linux version are not supported:
+
+    -a|--archive     same as -dpR --preserve=all
+    --attributes-only
+          don't copy the file data, just the attributes
+    --backup[=CONTROL]
+          make a backup of each existing destination file
+    -b               like --backup but does not accept an argument
+    --copy-contents  copy contents of special files when recursive
+    -l, --link       hard link files instead of copying
+    --preserve[=ATTR_LIST]
+          preserve the specified attributes (default: mode, ownership,
+          timestamps), if possible additional attributes: context, links,
+          xattr, all
+    --no-preserve=ATTR_LIST
+          don't preserve the specified attributes
+    --parents        use full source file name under DIRECTORY
+    --reflink[=WHEN]
+          control clone/CoW copies. See below
+    --remove-destination
+          remove each existing destination file before attempting to
+          open it (contrast with --force)
+    --sparse=WHEN    control creation of sparse files. See below
+    --strip-trailing-slashes
+          remove any trailing slashes from each SOURCE argument
+    -s, --symbolic-link
+          make symbolic links instead of copying
+    -S, --suffix=SUFFIX
+          override the usual backup suffix
+    -t, --target-directory=DIRECTORY
+          copy all SOURCE arguments into DIRECTORY
+    -T, --no-target-directory
+          treat DEST as a normal file
+    -u, --update     copy only when the SOURCE file is newer than the
+                     destination file or when the destination file is missing
+
+The option `-a` is supported, but will preserve only timestamps, no other attributes of the source files in the destination ones.
 
 ## Contributing
 
@@ -62,3 +148,9 @@ Licensed under the MIT license.
 [mkdir.js]: https://www.npmjs.com/package/@unixcompat/mkdir.js
 [rm.js]: https://www.npmjs.com/package/@unixcompat/rm.js
 [cat.js]: https://www.npmjs.com/package/@unixcompat/cat.js
+[POSIX documentation]: https://man7.org/linux/man-pages/man1/cp.1p.html
+[Linux implementation]: https://man7.org/linux/man-pages/man1/cp.1.html
+[`cp`]: https://nodejs.org/api/fs.html#fscpsrc-dest-options-callback
+[`copyFile`]: https://nodejs.org/api/fs.html#fscopyfilesrc-dest-mode-callback
+[node:fs]: https://nodejs.org/api/fs.html
+[BASH patterns]: https://www.linuxjournal.com/content/pattern-matching-bash
--- bin/cp.js
+++ bin/cp.js
@@ -1,32 +1,53 @@
 #!/usr/bin/env node
 
-function help() {
-  console.log(`${require('../package.json').description}
+import { access, cp, mkdir, readdir, realpath, stat } from 'fs/promises'
+import { constants, readFileSync } from 'fs'
+import { fileURLToPath } from 'url'
+import { basename, dirname, join } from 'path'
 
-Usage: cp.js [-DfHnpRrv] [--] src... dest
+function getPackage() {
+  const __dirname = dirname(fileURLToPath(import.meta.url))
+  return JSON.parse(readFileSync(join(__dirname, '../package.json'), 'utf8'))
+}
+
+function help() {
+  console.log(`${getPackage().description}
+
+Usage: cp.js [-DfHLnPpRrv] [--] src... dest
 
 Options:
-  -c|--cwd <dir>      directory to start looking for the source files in
-  -D|--dry-run        only print path of each source file or directory
-  -f|--force          removes the destination if not writable
-  -H|--dereference    follows symbolic links, so that the files are copied
-  -n|--no-clobbering  prevents accidentally overwriting any files
-  -p|--preserve       preserves timestamps of the source files
-  -R|--recursive      copy directories recursively (also -r)
-  -v|--verbose        print path of each copied file or directory
-  -V|--version        print version number
-  -h|--help           print usage instructions
+  -c|--cwd <dir>              directory to start looking for the source files
+  -D|--dry-run                only print paths of source files or directories
+  -a|--archive                the same as -dpR
+  -d                          the same as -P
+  -f|--force                  removes the destination if not writable
+  -H|--dereference-args       follow command-line symbolic links in src
+  -L|--dereference            always follow symbolic links in src
+  -n|--no-clobbering          prevents accidentally overwriting any files
+  -P|--no-dereference         never follow symbolic links in src
+  -p|--preserve[=timestamps]  preserve timestamps of the source files
+  -r                          the same as -R
+  -R|--recursive              copy directories recursively
+  -v|--verbose                print path of each copied file or directory
+  -V|--version                print version number
+  -h|--help                   print usage instructions
 
 Examples:
   $ cp.js prog.js prog.bak
   $ cp.js jones smith /home/nick/clients
-  $ cp.js -R /home/nick/clients /home/nick/customers`)
+  $ cp.js -R /home/nick/clients/ /home/nick/customers`)
 }
 
 const { argv } = process
 const args = []
-let   force = false, clobbering = true, dereference = false, 
-      preserveTimestamps = false, recursive = false, verbose, dry, cwd
+let   force = false, clobbering = true, dereference = false,
+      dereferenceArgs = false, preserveTimestamps = false,
+      recursive = false, verbose, dry, cwd
+
+function fail(message) {
+  console.error(message)
+  process.exit(1)
+}
 
 for (let i = 2, l = argv.length; i < l; ++i) {
   const arg = argv[i]
@@ -43,14 +64,23 @@ for (let i = 2, l = argv.length; i < l; ++i) {
         case 'f': case 'force':
           force = flag
           return
-        case 'H': case 'dereference':
+        case 'H': case 'dereference-args':
+          dereferenceArgs = flag
+          dereference = false
+          return
+        case 'L': case 'dereference':
           dereference = flag
+          dereferenceArgs = flag
           return
         case 'clobbering':
           clobbering = flag
           return
         case 'n':
           clobbering = !flag
+          return
+        case 'd': case 'P':
+          dereference = false
+          dereferenceArgs = false
           return
         case 'p': case 'preserve':
           preserveTimestamps = flag
@@ -62,15 +92,14 @@ for (let i = 2, l = argv.length; i < l; ++i) {
           verbose = flag
           return
         case 'V': case 'version':
-          console.log(require('../package.json').version)
+          console.log(getPackage().version)
           process.exit(0)
           break
         case 'h': case 'help':
           help()
           process.exit(0)
       }
-      console.error(`unknown option: "${arg}"`)
-      process.exit(1)
+      fail(`unknown option: "${arg}"`)
     }
     if (match[1] === '-') {
       const flags = match[3].split('')
@@ -88,20 +117,27 @@ for (let i = 2, l = argv.length; i < l; ++i) {
 }
 
 if (args.length < 2) {
-  console.error('missing source and destination paths')
+  help()
   process.exit(1)
 }
 
-const formatErr = ({ message }) => {
-  if (message.startsWith('Target already exists: cp returned EEXIST (')) {
-    message = `EEXIST: ${message.substring(43, message.lastIndexOf(') '))}`
-  } else if (message.startsWith('Invalid src or dest: cp returned EINVAL (')) {
-    message = `EINVAL: ${message.substring(41, message.lastIndexOf(') '))}`
+let arg
+const formatMessage = ({ code, message }) => {
+  /* c8 ignore next 2 */
+  if (code === 'EISDIR' || code === 'ERR_FS_EISDIR') {
+    message = `EISDIR: "${arg}" is a directory`
+  } else if (code === 'EEXIST' || code === 'ERR_FS_CP_EEXIST') {
+    message = `EEXIST: "${arg}" already exists`
+  /* c8 ignore next 5 */
+  } else if (code === 'EINVAL' || code === 'ERR_FS_CP_EINVAL') {
+    message = `EINVAL: "${arg}" is invalid`
+  } else if (code === 'ENOENT') {
+    message = `ENOENT: "${arg}" does not exist`
   }
   return message
 }
 
-(async () => {
+try {
   const paths = []
   const files = []
   const patterns = args
@@ -111,55 +147,67 @@ const formatErr = ({ message }) => {
       files.push(src)
     })
   if (patterns.length) {
-    const glob = require('fast-glob', { cwd })
+    const glob = (await import('fast-glob')).default
     if (verbose) console.log(patterns.join('\n'))
-    paths.push(...await glob(patterns))
+    paths.push(...await glob(patterns, {
+      cwd, extglob: true, dot: true, onlyFiles: false, markDirectories: true,
+      followSymbolicLinks: !!(dereference || dereferenceArgs)
+    }))
   }
-  
-
-  const { constants } = require('fs')
-  const { access, cp, mkdir, stat } = require('fs/promises')
-  const { basename, dirname, join } = require('path')
 
   const dest = args[args.length - 1]
   let destDir
   try {
     destDir = (await stat(dest)).isDirectory()
   } catch (err) {
+    /* c8 ignore next */
     if (err.code !== 'ENOENT') throw err
   }
 
-  const copy = async (paths, keepPath) => {
-    for (const path of paths) {
-      if (verbose) console.log(path)
-      if (dry) continue
-      const srcPath = cwd ? join(cwd, path) : path
-      let destPath
-      if (keepPath) {
-        const srcDir = dirname(path)
-        if (srcDir !== '.') await mkdir(join(dest, srcDir))
-        destPath = join(dest, path)
+  const copyOne = async (srcPath, destPath) => {
+    try {
+      arg = srcPath
+      await cp(srcPath, destPath, {
+        force: false, errorOnExist: true, dereference, preserveTimestamps, recursive
+      })
+    } catch (err) {
+      if (err.code === 'ERR_FS_CP_EEXIST' && clobbering) {
+        if (!force) await access(destPath, constants.W_OK)
+        await cp(srcPath, destPath, { dereference, preserveTimestamps, recursive })
       } else {
-        destPath = destDir ? join(dest, basename(path)) : dest
-      }
-      try {
-        await cp(srcPath, destPath, {
-          force: false, errorOnExist: true, dereference, preserveTimestamps, recursive
-        })
-      } catch (err) {
-        if (err.code === 'ERR_FS_CP_EEXIST' && clobbering) {
-          if (!force) await access(destPath, constants.W_OK)
-          await cp(srcPath, destPath, { dereference, preserveTimestamps, recursive })
-        } else {
-          throw err
-        }
+        throw err
       }
     }
   }
 
-  await copy(files, false)
-  await copy(paths, true)
-})().catch(err => {
-  console.error(formatErr(err))
+  const copyAll = async (paths, keepPath) => {
+    for (const path of paths) {
+      if (verbose) console.log(path)
+      if (dry) continue
+      const srcPath = cwd ? join(cwd, path) : path
+      if (recursive && srcPath.endsWith ('/') || srcPath.endsWith ('\\')) {
+        const children = await readdir(srcPath, { withFileTypes: true })
+        if (!destDir) {
+          await mkdir(dest)
+          destDir = true
+        }
+        for (const child of children) {
+          const { name } = child
+          await copyOne(join(srcPath, name), join(dest, name))
+        }
+      } else {
+        const srcReal = !dereference && dereferenceArgs ?
+          await realpath(srcPath) : srcPath
+        const destPath = recursive && keepPath ? join(dest, path) :
+            destDir ? join(dest, basename(path)) : dest
+        await copyOne(srcReal, destPath)
+      }
+    }
+  }
+
+  await copyAll(files, false)
+  await copyAll(paths, true)
+} catch(err) {
+  console.error(formatMessage(err))
   process.exitCode = 1
-})
+}
